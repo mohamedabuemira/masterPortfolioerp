a93517529a44adf791aeea47cde22ff9d8d9f1d2
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-07-10 23:11:08 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -347,6 +347,19 @@ const degrees = {
         ],
         website_link: "http://cu.edu.eg",
     },
+     {
+      title: "Indian Institute of Information Technology Kurnool",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "iiitk_logo.png",
+      alt_name: "IIITDM Kurnool",
+      duration: "2016 - 2020",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+      ],
+      website_link: "http://iiitk.ac.in",
+    },
     {
       title: "Indiana University Bloomington",
       subtitle: "M.S. in Computer Science",
