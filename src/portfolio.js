7abb417addf7be6a0e6c40e266b22899d2a0f171
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-07-10 22:59:26 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -271,15 +271,16 @@ const skills = {
 };
 
 // Education Page
+// Education Page
 const competitiveSites = {
   competitiveSites: [
     {
-      siteName: "CairoUniversity",
-      iconifyClassname: "simple-icons:cairo_university",
+      siteName: "LeetCode",
+      iconifyClassname: "simple-icons:leetcode",
       style: {
-        color: "#005739",
+        color: "#F79F1B",
       },
-      profileLink: "https://www.linkedin.com/in/abuemira/",
+      profileLink: "https://leetcode.com/layman_brother/",
     },
     {
       siteName: "HackerRank",
@@ -289,10 +290,71 @@ const competitiveSites = {
       },
       profileLink: "https://www.hackerrank.com/layman_brother",
     },
-
+    {
+      siteName: "Codechef",
+      iconifyClassname: "simple-icons:codechef",
+      style: {
+        color: "#5B4638",
+      },
+      profileLink: "https://www.codechef.com/users/ashutosh_1919",
+    },
+    {
+      siteName: "Codeforces",
+      iconifyClassname: "simple-icons:codeforces",
+      style: {
+        color: "#1F8ACB",
+      },
+      profileLink: "http://codeforces.com/profile/layman_brother",
+    },
+    {
+      siteName: "Hackerearth",
+      iconifyClassname: "simple-icons:hackerearth",
+      style: {
+        color: "#323754",
+      },
+      profileLink: "https://www.hackerearth.com/@ashutosh391",
+    },
+    {
+      siteName: "Kaggle",
+      iconifyClassname: "simple-icons:kaggle",
+      style: {
+        color: "#20BEFF",
+      },
+      profileLink: "https://www.kaggle.com/laymanbrother",
+    },
   ],
 };
 
+const degrees = {
+  degrees: [
+    {
+      title: "Indian Institute of Information Technology Kurnool",
+      subtitle: "B.Tech. in Computer Engineering",
+      logo_path: "iiitk_logo.png",
+      alt_name: "IIITDM Kurnool",
+      duration: "2016 - 2020",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+      ],
+      website_link: "http://iiitk.ac.in",
+    },
+    {
+      title: "Indiana University Bloomington",
+      subtitle: "M.S. in Computer Science",
+      logo_path: "iu_logo.png",
+      alt_name: "Indiana University Bloomington",
+      duration: "2021 - 2023",
+      descriptions: [
+        "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
+        "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
+        "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+      ],
+      website_link: "https://www.indiana.edu/",
+    },
+  ],
+};
 const degrees = {
   degrees: [
     {
